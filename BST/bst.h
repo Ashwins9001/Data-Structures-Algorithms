3ecbbe48125b96c435232d47c1c0ec623b767ce3
ashwins9001 <nitromekid@hotmail.com>
2020-01-21 05:25:39 -0500

    Addnode and in-order traversal

--- BST/bst.h
+++ BST/bst.h
@@ -1,4 +1,5 @@
 #pragma once
+using namespace std;
 class BST {
 private:
 	struct node{
@@ -8,6 +9,43 @@ private:
 	};
 
 	node* root;
+	void addLeafPrivate(int dataIn, node* Ptr)
+	{
+		if (root == NULL)
+			root = CreateLeaf(dataIn);
+		else if (dataIn < Ptr->data)
+		{
+			if (Ptr->left != NULL)
+				addLeafPrivate(dataIn, Ptr->left); //recursively traverse down to left node, then check again 
+			else
+				Ptr->left = CreateLeaf(dataIn); //create a leaf, returns node ptr, left-member of existing node now points to newly created
+		}
+			
+		else if (dataIn > Ptr->data) 
+		{
+			if (Ptr->right != NULL)
+				addLeafPrivate(dataIn, Ptr->right);  
+			else
+				Ptr->right = CreateLeaf(dataIn); 
+		}
+		else //dataIn = data at node
+		{
+			cout << "Data in " << dataIn << " has already been added to tree " << endl;
+		}
+	}
+	void printInOrderPrivate(node* Ptr)
+	{
+		if (root != NULL)
+		{
+			if (Ptr->left != NULL)
+				printInOrderPrivate(Ptr->left);
+			cout << Ptr->data << " ";
+			if (Ptr->right != NULL)
+				printInOrderPrivate(Ptr->right);
+		}
+		else
+			cout << "Tree is empty" << endl;
+	}
 public:
 	BST(){root = NULL;}
 	node* CreateLeaf(int dataIn) 
@@ -19,5 +57,13 @@ public:
 		n->right = NULL;
 		//return pointer to node struct, which has defn of data and its left/right pointers 
 		return n;
+	}
+	void addLeaf(int dataIn)
+	{
+		addLeafPrivate(dataIn, root); //keep pointer assignment private
+	}
+	void printInOrder()
+	{
+		printInOrderPrivate(root);
 	}
 };
--- BST/main.cpp
+++ BST/main.cpp
@@ -3,8 +3,17 @@
 #include "bst.cpp"
 using namespace std;
 
-int main() {
-
-
-	return 0;
+int main(int argc, char* argv[]) {
+	char c;
+	int treeKeys[15] = { 5, 30, 22, 39, 18, 7, 11, 56, 93, 92, 33, 2, 3, 6, 24 };
+	BST myTree;
+	cout << "Printing the tree in order before adding numbers " << endl;
+	myTree.printInOrder();
+	for (int i = 0; i < 15; i++)
+	{
+		myTree.addLeaf(treeKeys[i]);
+	}
+	cout << "Printing the tree in order after adding numbers " << endl;
+	myTree.printInOrder();
+	cin >> c;
 }
