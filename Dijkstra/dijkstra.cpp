a52a9235ffc3e438e725d0ca0f0b9e5d17ea75a5
ashwins9001 <nitromekid@hotmail.com>
2020-01-26 05:08:57 -0500

    Added graph for testing

--- Dijkstra/dijkstra.cpp
+++ Dijkstra/dijkstra.cpp
@@ -37,7 +37,7 @@ public:
 		distances[startNode] = 0; //each element of vector stores respective distance for it (from source, startNode)
 
 		//setup set and continue until it empties and all shortest distances finalised
-		while (!path_set.empty())
+		while(!path_set.empty())
 		{
 			//"pop off" or erase first element with shortest distance from path_set
 			//path_set iterator goes to first element, deref for tuple 
@@ -56,12 +56,13 @@ public:
 				if (distances[node] + weight < distances[vertex])
 				{
 					//erase current distance-vertex pair if it exists
+					cout << "Node " << node << " has a lower weight than node " << vertex << " updating.. " << endl;
 					if (distances[vertex] != INF)
-						path_set.erase(path_set.find(make_pair(vertex, distances[vertex])));
+						path_set.erase(path_set.find(make_pair(distances[vertex], vertex)));
+					//update distance-vertex pair
+					distances[vertex] = distances[node] + weight;
+					path_set.insert(make_pair(distances[vertex], vertex));
 				}
-				//update distance-vertex pair
-				distances[vertex] = distances[node] + weight;
-				path_set.insert(make_pair(distances[vertex], vertex));
 			}
 		}
 		cout << "Minimum distances to remaining nodes from vertex " << startNode << endl;
@@ -71,3 +72,58 @@ public:
 		}
 	}
 };
+
+
+int main() {
+	char c;
+	Graph g(9);
+
+	// Gives some edges and weights to the vertices
+	// Add node 0
+	g.addEdge(0, 1, 4);
+	g.addEdge(0, 7, 8);
+
+	// Add node 1
+	g.addEdge(1, 0, 4);
+	g.addEdge(1, 2, 8);
+	g.addEdge(1, 7, 11);
+
+	// Add node 2
+	g.addEdge(2, 1, 8);
+	g.addEdge(2, 8, 2);
+	g.addEdge(2, 5, 4);
+	g.addEdge(2, 3, 7);
+
+	// Add node 3
+	g.addEdge(3, 2, 7);
+	g.addEdge(3, 5, 14);
+	g.addEdge(3, 4, 9);
+
+	// Add node 4
+	g.addEdge(4, 3, 9);
+	g.addEdge(4, 5, 10);
+
+	// Add node 5
+	g.addEdge(5, 6, 2);
+	g.addEdge(5, 3, 14);
+	g.addEdge(5, 4, 10);
+
+	// Add node 6
+	g.addEdge(6, 7, 1);
+	g.addEdge(6, 8, 6);
+	g.addEdge(6, 5, 2);
+
+	// Add node 7
+	g.addEdge(7, 0, 8);
+	g.addEdge(7, 1, 11);
+	g.addEdge(7, 8, 7);
+	g.addEdge(7, 6, 1);
+
+	// Add node 8
+	g.addEdge(8, 2, 2);
+	g.addEdge(8, 7, 7);
+	g.addEdge(8, 6, 6);
+
+	g.shortestPath(0);
+	cin >> c;
+}
